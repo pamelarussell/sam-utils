d71d5c110be72d98357eb2072876d79df165ffdd
Pamela Russell <Pamela@cu-biot-2-10.203.132.162.int.colorado.edu>
2018-03-28 11:19:57 -0600

    command line options

--- bam_cigar_spans.py
+++ bam_cigar_spans.py
@@ -22,10 +22,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--bam', action = 'store', dest = 'bam', required = True, help = 'Bam file')
 parser.add_argument('--out_counts', action = 'store', dest = 'out_counts', required = False, help = 'Output table of cigar span counts')
 parser.add_argument('--out_hist_prefix', action = 'store', dest = 'out_fig_prefix', required = False, help = 'Prefix for output histograms (one per reference sequence)')
+parser.add_argument('--extra_hist_label', action = 'store', dest = 'hist_label', required = False, help = 'Additional label to prepend to histogram titles')
 args = parser.parse_args()
 bam_file = args.bam
 out_span_counts = args.out_counts
 out_fig_prefix = args.out_fig_prefix
+hist_label = args.hist_label
 
 # Open the bam file and get an iterator over records
 print("Opening bam file:\n%s" % bam_file)
@@ -72,7 +74,10 @@ if out_fig_prefix is not None:
         fig_data = cigar_spans.loc[cigar_spans["ref"] == ref].as_matrix(columns = ["span"])
         print("Writing histogram of cigar spans for %s reads to file:\n%s" % (fig_data.shape[0], out_fig))
         n, bins, patches = plt.hist(fig_data, bins = 500, histtype = "stepfilled", cumulative = False)
-        plt.title(ref)
+        plt_title = ref
+        if hist_label is not None:
+            plt_title = "%s -> %s" % (hist_label, plt_title)
+        plt.title(plt_title)
         plt.xlabel("Cigar span")
         plt.ylabel("Number of reads")
         plt.savefig(out_fig)
