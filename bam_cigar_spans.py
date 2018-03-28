682269be1e3bce871d78917f3aee7fc9ad2fc53a
Pamela Russell <Pamela@cu-biot-2-10.203.132.162.int.colorado.edu>
2018-03-28 14:21:39 -0600

    write bam files of reads with span in each chunk

--- bam_cigar_spans.py
+++ bam_cigar_spans.py
@@ -1,12 +1,3 @@
-"""
-bam_cigar_spans.py
-
-This script reads a bam file and determines the span along the reference sequence
-covered by each record. A table is written that gives the number of records with
-each span for each reference sequence. For each reference sequence, a histogram
-of read spans is also saved. The output files are optional and are not generated
-if output file paths are not specified.
-"""
 
 import argparse
 import re
@@ -18,44 +9,86 @@ import pandas as pd
 from sam_utils import cigar_span
 
 # Parse the command line
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description = """
+bam_cigar_spans.py
+ 
+This script reads a bam file and determines the span along the reference sequence
+covered by each record. It creates three sets of outputs:
+
+1. A table giving the number of records with each span for each reference sequence
+2. A histogram of read spans for each reference sequence
+3. Bam files consisting only of reads whose span is in a particular length interval
+""")
 parser.add_argument('--bam', action = 'store', dest = 'bam', required = True, help = 'Bam file')
-parser.add_argument('--out_counts', action = 'store', dest = 'out_counts', required = False, help = 'Output table of cigar span counts')
-parser.add_argument('--out_hist_prefix', action = 'store', dest = 'out_fig_prefix', required = False, help = 'Prefix for output histograms (one per reference sequence)')
-parser.add_argument('--extra_hist_label', action = 'store', dest = 'hist_label', required = False, help = 'Additional label to prepend to histogram titles')
+parser.add_argument('--out_counts', action = 'store', dest = 'out_counts', required = True, help = 'Output table of cigar span counts')
+parser.add_argument('--out_hist_prefix', action = 'store', dest = 'out_fig_prefix', required = True, help = 'Prefix for output histograms (one per reference sequence)')
+parser.add_argument('--extra_hist_label', action = 'store', dest = 'hist_label', required = True, help = 'Additional label to prepend to histogram titles')
+parser.add_argument('--max_read_span_out', action = 'store', dest = 'max_span', required = True, help = 'Max read span to write to chunk bam files')
+parser.add_argument('--chunk_size_out', action = 'store', dest = 'chunk_size', required = True, help = 'Size of chunks (length interval)')
 args = parser.parse_args()
 bam_file = args.bam
 out_span_counts = args.out_counts
 out_fig_prefix = args.out_fig_prefix
 hist_label = args.hist_label
-
-# Open the bam file and get an iterator over records
-print("Opening bam file:\n%s" % bam_file)
-bam_reader = pysam.AlignmentFile(bam_file, "rb")
-bam_iter = bam_reader.fetch()
+max_len_wb = args.max_span
+chunk_size_wb = args.chunk_size
 
 # Determine the number of mapped reads in the bam file
 n_mapped = int(pysam.view("-c", "-F", "4", bam_file))
 n_unmapped = int(pysam.view("-c", "-f", "4", bam_file))
 print("\nThere are %s mapped reads and %s unmapped reads." % (n_mapped, n_unmapped))
 
+# Construct cigar length chunks for separate bam files
+len_chunks = []
+begin = 0
+end = chunk_size_wb
+while end <= max_len_wb:
+    len_chunks.append((begin, end))
+    begin = end
+    end = begin + chunk_size_wb
+if end < max_len_wb:
+    len_chunks.append((end, max_len_wb))
+# Map of cigar length to length chunk
+len_to_chunk = {l: t for t in len_chunks for l in range(t[0], t[1])}
+
+# Open the bam file and get the header
+bam_reader = pysam.AlignmentFile(bam_file, "rb")
+header = bam_reader.header
+
+# Make a bam file writer for each length range/chunk
+def chunk_to_bam(chunk):
+    return "%s%s_%s.bam" % (out_bam_prefix, chunk[0], chunk[1])
+bam_writers = {chunk: pysam.AlignmentFile(chunk_to_bam(chunk), "wb", header = header) for chunk in len_chunks}
+
 # Keep track of cigar spans
 cigar_spans = pd.DataFrame(columns = ["ref", "span"], index = range(n_mapped))
 
-# For each read, record the cigar span in the map corresponding to its reference sequence
+# Iterate through bam file and save cigar spans
+# Write bam files of records with cigar span in each interval
 print("\nIterating through bam file and getting cigar spans...")
+bam_iter = bam_reader.fetch()
 i = 0
 for rec in bam_iter:
+    if i == 1000:
+        break
     ref = rec.reference_name
     span = cigar_span(rec.cigartuples)
+    # Append the record to the appropriate bam file
+    bam_writers[len_to_chunk[span]].write(rec)
     cigar_spans.iloc[i] = [ref, span]
     i = i + 1
     if i % 10000 == 0:
         print("Finished %s records" % i)
 
-# Close the bam file
+# Close the bam files
 bam_reader.close()
+for bam_writer in bam_writers.values():
+    bam_writer.close()
 
+# Index the chunk bam files
+for chunk in len_chunks:
+    pysam.index(chunk_to_bam(chunk))
+    
 # Write the span counts to a table
 if out_span_counts is not None:
     print("\nWriting span counts to file:\n%s" % out_span_counts)
