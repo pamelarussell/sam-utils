061b521dcc87004399489f64b13b4266b5433cc4
Pamela Russell <pamela.russell@gmail.com>
2018-05-03 11:16:49 -0600

    better control over plotting of histograms

--- bam_cigar_spans.py
+++ bam_cigar_spans.py
@@ -160,18 +160,21 @@ if out_fig_prefix is not None:
         logger.write("Not writing histograms because there are too many (%s) reference sequences to plot" % len(refs_to_plot))
     else:
         for ref in refs_to_plot:
-            out_fig = "%s%s.pdf" % (out_fig_prefix, re.sub("[| .]", r'_', ref))       
-            logger.write("Writing histogram of cigar spans to file:\n%s\n" % out_fig)
-            plt_data = [to_log(x) for x in cigar_span_counts[ref].values()]
-            plt.bar(span_keys, plt_data)
-            plt_title = ref
-            if hist_label is not None:
-                plt_title = "%s -> %s" % (hist_label, plt_title)
-            plt.title(plt_title)
-            plt.xlabel("Cigar span")
-            plt.ylabel("Number of reads (log10)")
-            plt.savefig(out_fig)
-    
+            if ref in cigar_span_counts:
+                out_fig = "%s%s.pdf" % (out_fig_prefix, re.sub("[| .]", r'_', ref))       
+                logger.write("Writing histogram of cigar spans to file: %s\n" % out_fig)
+                plt_data = [to_log(x) for x in cigar_span_counts[ref].values()]
+                plt.bar(span_keys, plt_data)
+                plt_title = ref
+                if hist_label is not None:
+                    plt_title = "%s -> %s" % (hist_label, plt_title)
+                plt.title(plt_title)
+                plt.xlabel("Cigar span")
+                plt.ylabel("Number of reads (log10)")
+                plt.savefig(out_fig)
+            else:
+                logger.write("Skipping reference %s: not found.\n" % ref)    
+
     
 logger.write("\nAll done.\n\n")
 logger.close()    
